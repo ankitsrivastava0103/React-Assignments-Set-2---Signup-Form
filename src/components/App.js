2d9dd787b69b487e4c57d3a73e2d288d285f9665
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2020-12-04 20:29:48 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,13 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [inputFields, setInputFields] = useState({
+    name: "",
+    emailID: "",
+    gender: "male",
+    phoneNumber: "",
+    password: ""
+  });
   return (
     <div id="main">
       <form>
@@ -10,30 +17,35 @@ const App = () => {
           type="text"
           name="name"
           placeholder="Enter Your Name"
+          value={inputFields.name}
         ></input>
         <input
           data-testid="email"
           type="email"
           name="email"
           placeholder="Enter Your Email Id"
+          value={inputFields.emailID}
         ></input>
         <input
           data-testid="gender"
           type="text"
           name="gender"
           placeholder="Enter Your Gender (male or female or other)"
+          value={inputFields.gender}
         ></input>
         <input
           data-testid="phoneNumber"
           type="number"
           name="phoneNumber"
           placeholder="Enter Your Phone Number"
+          value={inputFields.phoneNumber}
         ></input>
         <input
           data-testid="password"
           type="password"
           name="password"
           placeholder="Enter Your Password"
+          value={inputFields.password}
         ></input>
         <button data-testid="submit">Submit</button>
       </form>
